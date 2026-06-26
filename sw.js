ec6c0a1580263c6e69be9991318933fd2116c6ab
akshayjs1998 <akshayjs1998@gmail.com>
2026-06-26 19:04:44 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'lyffin-ops-v2';
+const CACHE = 'lyffin-ops-v1782480813';
 const FILES = [
   '/lyffin-ops/login.html',
   '/lyffin-ops/dashboard.html',
@@ -11,21 +11,6 @@ const FILES = [
   '/lyffin-ops/icon-192.png',
   '/lyffin-ops/icon-512.png'
 ];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
-});
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))); self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
+self.addEventListener('fetch', e => { e.respondWith(fetch(e.request).catch(() => caches.match(e.request))); });
